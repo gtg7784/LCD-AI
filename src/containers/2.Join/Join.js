f03b4cc624616e801fbc2d81a1c1a946844a2d41
Go Tae Geon <gtg7784@lab-c.me>
2019-03-10 15:53:25 +0900

    UPDATE path

--- src/containers/2.Join/Join.js
+++ src/containers/2.Join/Join.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import * as classNames from 'classnames/bind';
 import styles from './Join.scss';
-import logo from '../logo.png'
+import logo from '../../assets/logo.png'
 
 const cx = classNames.bind(styles);
 
