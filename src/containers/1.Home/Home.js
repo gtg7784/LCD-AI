77f8f4be1f88cdb0f3d44c030f418f515d2f4429
Go Tae Geon <gtg7784@lab-c.me>
2019-03-10 15:53:19 +0900

    FIX error & UPDATE path

--- src/containers/1.Home/Home.js
+++ src/containers/1.Home/Home.js
@@ -3,8 +3,8 @@ import * as React from 'react';
 import * as classNames from 'classnames/bind';
 import styles from './Home.scss';
 import {Link} from "react-router-dom";
-import drag from './drag.png';
-import logo from '../logo.png';
+import drag from '../../assets/drag.png';
+import logo from '../../assets/logo.png';
 const cx = classNames.bind(styles);
 
 export default class Home extends React.Component {
@@ -22,8 +22,11 @@ export default class Home extends React.Component {
                         </ul>
                     </nav>
                     <div>
-                        <img src={drag}/> <br/>
-                        <Link to="/"><input type="file"  accept=".jpg, .png, .dcm, .dicom">파일 선택하기</input></Link>
+                        <img src={drag} alt=""/> <br/>
+                        <Link to="/">
+                            <input type="file" accept=".jpg, .png, .dcm, .dicom" />
+                            <label htmlFor="">파일 선택하기</label>
+                        </Link>
                     </div>
                     <div>
                         파일 업로드시 본 약관을 동의하는 것으로 간주합니다. <span><Link to='/term'>(약관 바로가기)</Link></span>
