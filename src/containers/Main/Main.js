079448176b55f40b510e001b2d1b0e9ca4736457
Go Tae Geon <gtg7784@lab-c.me>
2019-03-13 23:34:30 +0900

    CLEAR Main

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -10,31 +10,6 @@ export default class Main extends React.Component {
     render() {
         return (
             <div className={cx('Main')}>
-                <div/>
-                <header className={cx("loginHeader")}>
-                    <form>
-                        <img src={logo} alt="logo"/> <br/>
-                        <p>
-                            <div>
-                                <span>이름</span>
-                                <input type="text"/>
-                                <span>생년월일</span>
-                                <input type="date"/>
-                                <span>소속병원</span>
-                                <input type="text"/>
-                            </div>
-                            <div>
-                                <span>이메일 아이디</span>
-                                <input type="email"/>
-                                <span>비밀번호</span>
-                                <input type="password"/>
-                                <span>비밀번호 확인</span>
-                                <input type="password"/>
-                            </div>
-                        </p>
-                        <Link to='login'><button>가입하기</button></Link>
-                    </form>
-                </header>
             </div>
         );
     }
